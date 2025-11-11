289766394ed15cec8166b3ae90a275d3e42e9b4e
kishor-pandit <kishorpandit309@gmail.com>
2025-11-11 07:29:58 +0545

    Add files via upload

---
+++ c.php
@@ -0,0 +1,16 @@
+<?php
+$studentProfile = [
+    'name'        => 'Kishor Pandit',
+    'roll_number' => 19,
+    'faculty'     => 'BCSIT',
+    'semester'    => 3,
+    'email'       => 'kishorpandit309@gmail.com'
+];
+
+echo "STUDENT PROFILE\n";
+echo "Name: " . $studentProfile['name'] . "\n";
+echo "Roll Number: " . $studentProfile['roll_number'] . "\n";
+echo "Faculty: " . $studentProfile['faculty'] . "\n";
+echo "Semester: " . $studentProfile['semester'] . "\n";
+echo "Email: " . $studentProfile['email'] . "\n";
+?>
---
+++ s.php
@@ -0,0 +1,8 @@
+<?php
+function introduce($name, $age, $city){
+    echo "My name is $name. <br> I am $age years old. <br> I live in $city.<br><br>";
+}
+introduce("heroye", 20, "Tokyo");
+introduce("Hirey", 22, "Kyoto");
+introduce("Renim", 19, "Hiroshima");
+?>
